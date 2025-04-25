5ef74476fe81af7ae7a3b4c4c05ae84235d2258b
Montek <87750128+Montekkundan@users.noreply.github.com>
2025-04-24 23:47:04 -0700

    dummy 64-character hex string

--- Formula/yo-bin.rb
+++ Formula/yo-bin.rb
@@ -5,10 +5,10 @@ class YoBin < Formula
 
   if OS.mac?
     url "https://github.com/Montekkundan/yo/releases/download/1.0.10/yo-1.0.10-x86_64-apple-darwin.tar.gz"
-    sha256 "REPLACE_WITH_MAC_SHA256"
+    sha256 "0000000000000000000000000000000000000000000000000000000000000000"
   elsif OS.linux?
     url "https://github.com/Montekkundan/yo/releases/download/1.0.10/yo-1.0.10-x86_64-unknown-linux-musl.tar.gz"
-    sha256 "REPLACE_WITH_LINUX_SHA256"
+    sha256 "0000000000000000000000000000000000000000000000000000000000000000"
   end
 
   def install
